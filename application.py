058d6f8cbf254cdd38d4f6cdc33ae47d22e62971
peng <you@example.com>
2020-03-29 22:02:56 +0800

    update demo address

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
+import os
 import tornado
 from urls import urls
 
 application = tornado.web.Application(
               handlers=urls,
-              settings=dict(template_path='./templates', static_path='./static'),
+              settings=dict(template_path='./templates'),
+              static_path= os.path.join(os.path.dirname(__file__), "static"),
               debug=True
               )
